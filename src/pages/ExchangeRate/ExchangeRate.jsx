6a63956adda413f1661ea7aac83787c96cb201ef
sky <harutazatyan45@gmail.com>
2025-12-02 12:42:10 +0400

    mmaaaaա

--- src/pages/ExchangeRate/ExchangeRate.jsx
+++ src/pages/ExchangeRate/ExchangeRate.jsx
@@ -22,6 +22,7 @@ const ExchangeRate = () => {
       .then(setData)
       .catch((err) => console.error("Failed to fetch exchange rates", err));
   }, [base]);
+  
 
   useEffect(() => {
     if (!data) return;
